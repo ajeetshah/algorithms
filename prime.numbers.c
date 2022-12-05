f7062b5019ba675feffa70f83e84314c1a18acd9
Ajeet <ajeets.shah@hotmail.com>
2022-12-05 22:53:07 +0530

    Add solution of primes numbers

--- prime.numbers.c
+++ prime.numbers.c
@@ -1,28 +1,46 @@
 #include<stdio.h>
+#include<math.h>
 #define SIZE 1200
 
-int main() {
-	int primes[SIZE] = {0};
+int *get_primes(int *primes) {
 
 	int i = 0;
 	primes[i] = 2;
-	primes[++i] = 3;
+	i++;
+
+	int j = 0;
+	int number = 3;
 
 	while(1) {
-		printf("Inside while loop\n");
 		if (i == SIZE) {
 			break;	
 		}
-		// 
-		//
+		
+		if (sqrt(number) < (double) primes[j]) { // a prime
+			primes[i] = number;
+			i++;
+			number++;
+			j = 0;
+			continue;
+		}
 
-		i++;
+		if (number % primes[j] == 0) { // not a prime
+			number++;
+			j = 0;
+			continue;
+		}
+		j++;
 	}
 
-	for (int i=0; i<SIZE; i++) {
-		printf("Prime number: %d, ", primes[i]);
-	}
-
-	return 0;
+	return primes;
 }
 
+int main() {
+	int *p;
+	int primes[SIZE];
+	p = get_primes(primes);
+	for (int i=0; i<SIZE; i++) {
+                printf("Prime number: %d\n", p[i]);
+        }
+	return 0;
+}
